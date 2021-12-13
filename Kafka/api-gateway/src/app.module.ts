a75a096138aa17e1dfe448e9de5a3756f1eabfa2
radrigis <mr.rodion_oa@mail.ri>
2021-12-14 00:04:06 +0300

    consumer update

--- Kafka/api-gateway/src/app.module.ts
+++ Kafka/api-gateway/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PostsModule } from './posts/posts.module';
+import { PostsController } from './posts/posts.controller';
 
 @Module({
-  imports: [PostsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [],
+  controllers: [PostsController],
+  providers: [],
 })
 export class AppModule {}
